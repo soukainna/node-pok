d0965cd554b3949e90e2acf2b77d9c18b78a68ef
soukainna <soukaina1lafdili@gmail.com>
2022-08-02 23:47:46 +0200

    add many route

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ app.get('/', (require, response) => {
     response.send('hello express word')
 })
 
-app.get('/api/pokemons/1', (req, res) => { res.send('hello, Bulbizare !')})
+// app.get('/api/pokemons/1', (req, res) => { res.send('hello, Bulbizare !')})
+//ici je vais essayer d'implementer le code pour aficher plusieurs root
 
-app.listen(port, () => { console.log('notre app Node est demarrée sur http://localhost:${port}')})
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    res.send(`Vous avez demander la root du pokemon ${id}`)
+})
+
+app.listen(port, () => { console.log(`notre app Node est demarrée sur http://localhost:${port}`)})
