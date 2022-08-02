60d75bc3a64dfd421099bb5f964d7bddc3f84bcb
soukainna <soukaina1lafdili@gmail.com>
2022-08-03 00:26:40 +0200

    add res JSON

--- app.js
+++ app.js
@@ -13,11 +13,18 @@ app.get('/', (require, response) => {
 //ici je vais essayer d'implementer le code pour aficher plusieurs root
 
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+    const id = parseInt(req.params.id) // j'ai ajouter parseInt parce que express par defaut envoie tous en string
     console.log(id)
     const pokemon = pokemons.find(pokemonn => pokemonn.id === id)
     // res.send(`Vous avez demander la root du pokemon ${id}`)
-    res.send(`Vous avez demander la root du pokemon ${pokemon.name}`)
+    // res.send(`Vous avez demander la root du pokemon ${pokemon.name}`)
+    //je vais essayer d'envoyer du JSON
+    res.json(pokemon)
+})
+
+app.get('/api/pokemons', (req, res) => {
+    const result = pokemons.length
+    res.send(`Il y a ${result} pokemons`)
 })
 
 app.listen(port, () => { console.log(`notre app Node est demarrée sur http://localhost:${port}`)})
