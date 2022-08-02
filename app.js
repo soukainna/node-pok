434773b0c1c1fdf1de1b46718ac18f32448bf5d4
soukainna <soukaina1lafdili@gmail.com>
2022-08-03 00:03:54 +0200

    fixe err express

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+let pokemons = require('./pok-list')
 
 const app = express()
 const port = 3000
@@ -12,9 +13,11 @@ app.get('/', (require, response) => {
 //ici je vais essayer d'implementer le code pour aficher plusieurs root
 
 app.get('/api/pokemons/:id', (req, res) => {
-    const id = req.params.id
+    const id = parseInt(req.params.id)
     console.log(id)
-    res.send(`Vous avez demander la root du pokemon ${id}`)
+    const pokemon = pokemons.find(pokemonn => pokemonn.id === id)
+    // res.send(`Vous avez demander la root du pokemon ${id}`)
+    res.send(`Vous avez demander la root du pokemon ${pokemon.name}`)
 })
 
 app.listen(port, () => { console.log(`notre app Node est demarrée sur http://localhost:${port}`)})
