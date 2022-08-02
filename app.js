19d24d859dffacb6c0567c0c97d317b7e3183b12
soukainna <soukaina1lafdili@gmail.com>
2022-08-02 23:29:30 +0200

    add nodemon pour run my app

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (require, response) => {
-    response.send('hello express ')
+    response.send('hello express word')
 })
+
+app.get('/api/pokemons/1', (req, res) => { res.send('hello, Bulbizare !')})
 
 app.listen(port, () => { console.log('notre app Node est demarrée sur http://localhost:${port}')})
