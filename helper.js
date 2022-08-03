9925f7437fc7da14aea43558aa7d51986274f647
soukainna <soukaina1lafdili@gmail.com>
2022-08-03 11:24:30 +0200

    add helper.js

---
+++ helper.js
@@ -0,0 +1,12 @@
+// const success = (massage, data) => {
+//     return {message: message,
+//         data: data}
+// }
+
+// exports.success
+
+//le racourci avec ECMASCRIPT 6
+
+exports.success = (message, data) => {
+    return{message, data }
+}
